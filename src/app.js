c75acae8a732656e18fbcb702640f64590ffbc12
TouseerAmir <touseeramir@gmail.com>
2024-07-27 09:51:54 +0500

    All setup configured

--- src/app.js
+++ src/app.js
@@ -11,3 +11,5 @@ app.use(express.urlencoded({extended:true,limit:"10kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 export { app };
+import userRouter from './routes/user.routes.js';
+app.use('/api/v1/users',userRouter)
